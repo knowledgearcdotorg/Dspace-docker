be98b3517b265bdc78cb2432e5cb42dbe905b2b0
Jonathan Kosgei <jonathan@saharacluster.com>
2017-08-19 16:48:58 +0300

    ant fresh_install on startup

--- dspace/entry.sh
+++ dspace/entry.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd /opt/dspace-6.1-src-release/dspace/target/dspace-installer
+
+ant fresh_install
+
 psql -h postgres -p 5432 --username=postgres dspace -c "CREATE EXTENSION pgcrypto;"
 
 /opt/apache-tomcat-9.0.0.M26/bin/catalina.sh
