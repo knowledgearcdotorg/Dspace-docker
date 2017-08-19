d5c5c27c4390adc2272ce4b4dcb571bb0ff1c5a6
Jonathan Kosgei <jonathan@saharacluster.com>
2017-08-19 17:58:06 +0300

    pycrypto before ant fresh_install

--- dspace/entry.sh
+++ dspace/entry.sh
@@ -2,8 +2,8 @@
 
 cd /opt/dspace-6.1-src-release/dspace/target/dspace-installer
 
-ant fresh_install
-
 psql -h postgres -p 5432 --username=postgres dspace -c "CREATE EXTENSION pgcrypto;"
+
+ant fresh_install
 
 /opt/apache-tomcat-9.0.0.M26/bin/catalina.sh
