5571eb523df03c57f887403abf949cbff9891af9
Jonathan Kosgei <jonathan@saharacluster.com>
2017-08-19 19:44:13 +0300

    change hostname to localhost

--- dspace/entry.sh
+++ dspace/entry.sh
@@ -15,6 +15,8 @@ psql -h postgres -p 5432 --username=dspace dspace -c "CREATE EXTENSION pgcrypto;
 
 ant fresh_install
 
+echo "Copying webapps to tomcat..."
 cp -r /opt/dspace/webapps/xmlui /opt/dspace/webapps/oai /opt/dspace/webapps/solr /opt/dspace/webapps/rest /opt/apache-tomcat-9.0.0.M26/webapps
+echo "Completed copying webapps."
 
 /opt/apache-tomcat-9.0.0.M26/bin/catalina.sh run
