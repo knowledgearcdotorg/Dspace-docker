f81278a50ae3bf1199000e73fc0e19f4edcc7703
Jonathan Kosgei <jonathan@saharacluster.com>
2017-08-20 13:19:52 +0300

    inspect dir

--- dspace/entry.sh
+++ dspace/entry.sh
@@ -19,6 +19,7 @@ echo "I am "$(whoami)
 
 ls -l /opt/
 ls -l /opt/apache-tomcat-8.0.45
+ls -l /opt/apache-tomcat-8.0.45/conf/Catalina
 
 echo '<?xml version="1.0" ?>
 <Context name="" docBase="/opt/dspace/webapps/xmlui" reloadable="true" crossContext="true">
