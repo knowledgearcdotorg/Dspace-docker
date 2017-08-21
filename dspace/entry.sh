0006c60c0cdc9eb21226b933eb6c490b324a0261
Jonathan Kosgei <jonathan@saharacluster.com>
2017-08-21 12:30:54 +0300

    tomcat 8.0.46

--- dspace/entry.sh
+++ dspace/entry.sh
@@ -17,30 +17,30 @@ ant fresh_install
 
 echo "I am "$(whoami)
 
-mkdir -p /opt/apache-tomcat-8.0.45/conf/Catalina/localhost
+mkdir -p /opt/apache-tomcat-8.0.46/conf/Catalina/localhost
 
 echo '<?xml version="1.0" ?>
 <Context name="" docBase="/opt/dspace/webapps/xmlui" reloadable="true" crossContext="true">
     <WatchedResource>WEB-INF/web.xml</WatchedResource>
-</Context>' > /opt/apache-tomcat-8.0.45/conf/Catalina/localhost/ROOT.xml
+</Context>' > /opt/apache-tomcat-8.0.46/conf/Catalina/localhost/ROOT.xml
 
 echo '<?xml version="1.0" ?>
 <Context name="/rest" docBase="/opt/dspace/webapps/rest" reloadable="true" crossContext="true">
     <WatchedResource>WEB-INF/web.xml</WatchedResource>
-</Context>' > /opt/apache-tomcat-8.0.45/conf/Catalina/rest.xml
+</Context>' > /opt/apache-tomcat-8.0.46/conf/Catalina/rest.xml
 
 echo '<?xml version="1.0" ?>
 <Context name="/oai" docBase="/opt/dspace/webapps/oai" reloadable="true" crossContext="true">
     <WatchedResource>WEB-INF/web.xml</WatchedResource>
-</Context>' > /opt/apache-tomcat-8.0.45/conf/Catalina/oai.xml
+</Context>' > /opt/apache-tomcat-8.0.46/conf/Catalina/oai.xml
 
 echo '<?xml version="1.0" ?>
 <Context name="/solr" docBase="/opt/dspace/webapps/solr" reloadable="true" crossContext="true">
     <WatchedResource>WEB-INF/web.xml</WatchedResource>
-</Context>' > /opt/apache-tomcat-8.0.45/conf/Catalina/solr.xml
+</Context>' > /opt/apache-tomcat-8.0.46/conf/Catalina/solr.xml
 
 #echo "Copying webapps to tomcat..."
-#cp -r /opt/dspace/webapps/xmlui /opt/dspace/webapps/oai /opt/dspace/webapps/solr /opt/dspace/webapps/rest /opt/apache-tomcat-8.0.45/webapps
+#cp -r /opt/dspace/webapps/xmlui /opt/dspace/webapps/oai /opt/dspace/webapps/solr /opt/dspace/webapps/rest /opt/apache-tomcat-8.0.46/webapps
 #echo "Completed copying webapps."
 
-/opt/apache-tomcat-8.0.45/bin/catalina.sh run
+/opt/apache-tomcat-8.0.46/bin/catalina.sh run
