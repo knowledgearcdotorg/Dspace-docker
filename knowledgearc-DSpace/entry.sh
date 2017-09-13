3fd6d239077e0acadd89bd5224b70b11e4543afe
Jonathan Kosgei <jonathan@saharacluster.com>
2017-09-13 20:56:45 +0300

    fix webapps path

--- knowledgearc-DSpace/entry.sh
+++ knowledgearc-DSpace/entry.sh
@@ -217,22 +217,22 @@ echo "I am "$(whoami)
 mkdir -p /opt/apache-tomcat-${TOMCAT_MINOR}/conf/Catalina/localhost
 
 echo '<?xml version="1.0" ?>
-<Context name="" docBase="/dspace/webapps/xmlui" reloadable="true" crossContext="true">
+<Context name="" docBase="/opt/dspace/webapps/xmlui" reloadable="true" crossContext="true">
     <WatchedResource>WEB-INF/web.xml</WatchedResource>
 </Context>' > /opt/apache-tomcat-${TOMCAT_MINOR}/conf/Catalina/localhost/ROOT.xml
 
 echo '<?xml version="1.0" ?>
-<Context name="/rest" docBase="/dspace/webapps/rest" reloadable="true" crossContext="true">
+<Context name="/rest" docBase="/opt/dspace/webapps/rest" reloadable="true" crossContext="true">
     <WatchedResource>WEB-INF/web.xml</WatchedResource>
 </Context>' > /opt/apache-tomcat-${TOMCAT_MINOR}/conf/Catalina/localhost/rest.xml
 
 echo '<?xml version="1.0" ?>
-<Context name="/oai" docBase="/dspace/webapps/oai" reloadable="true" crossContext="true">
+<Context name="/oai" docBase="/opt/dspace/webapps/oai" reloadable="true" crossContext="true">
     <WatchedResource>WEB-INF/web.xml</WatchedResource>
 </Context>' > /opt/apache-tomcat-${TOMCAT_MINOR}/conf/Catalina/localhost/oai.xml
 
 echo '<?xml version="1.0" ?>
-<Context name="/solr" docBase="/dspace/webapps/solr" reloadable="true" crossContext="true">
+<Context name="/solr" docBase="/opt/dspace/webapps/solr" reloadable="true" crossContext="true">
     <WatchedResource>WEB-INF/web.xml</WatchedResource>
 </Context>' > /opt/apache-tomcat-${TOMCAT_MINOR}/conf/Catalina/localhost/solr.xml
 
